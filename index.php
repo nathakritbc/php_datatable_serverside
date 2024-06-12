9cf2605eeec18ac2086b510455c58fad01e15d1d
Nathakrit Thitsatian <63659324+nathakritbc@users.noreply.github.com>
2024-06-12 21:55:13 +0700

    update index page

--- index.php
+++ index.php
@@ -4,14 +4,14 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>DataTable Server-Side Processing</title>
+    <title>DataTable Server-Side</title>
     <link rel="stylesheet" href="https://cdn.datatables.net/1.11.3/css/dataTables.bootstrap5.min.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" />
 </head>
 
 <body>
     <div class="container my-5">
-        <h2 class="my-4 text-primary text-center">DataTable Server-Side Processing</h2>
+        <h2 class="my-4 text-primary text-center">DataTable Server-Side</h2>
         <div class="px-3">
             <h4 class="my-4">User Data</h4>
             <table id="userTable" class="table border table-responsive" style="width:100%">
